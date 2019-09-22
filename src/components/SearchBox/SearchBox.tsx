b439ffeb4f5ce3c9c642356f1722cc4815d93556
neal.nankivell <neal.nankivell@gmail.com>
2019-09-22 13:26:53 +0200

    Added styles to the SearchBox

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { SearchBoxProps } from "./SearchBoxProps";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  background-color: #ffffff20;
+  border: none;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  width: 20em;
+  max-width: 600px;
+  color: #fff;
+  padding-left: 1em;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  padding-left: 2em;
+  padding-right: 2em;
+`;
 
 export const SearchBox: React.FC<SearchBoxProps> = props => {
   return (
-    <input
-      onChange={e => props.onChange(e.target.value)}
-      defaultValue={props.value}
-    />
+    <Wrapper>
+      <StyledInput
+        onChange={e => props.onChange(e.target.value)}
+        defaultValue={props.value}
+        placeholder="Search"
+      />
+    </Wrapper>
   );
 };
 
