151fa161f68e6e74dfd5f730c0b525406717b45e
neal.nankivell <neal.nankivell@gmail.com>
2019-09-23 14:21:46 +0200

    Configured A11y in storybook

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,9 +1,11 @@
 import "../src/index.css";
 import { configure, addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 
 const req = require.context("../src", true, /\.stories.tsx$/);
 addDecorator(withKnobs);
+addDecorator(withA11y);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
