5dfcaddc1092f14438445898ef0695e14627a5c1
neal.nankivell <neal.nankivell@gmail.com>
2019-10-03 14:15:35 +0200

    Adds test to verify the SearchBox is rendered correctly

--- src/components/SearchBox/SearchBox.test.tsx
+++ src/components/SearchBox/SearchBox.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchBox from "./SearchBox";
+import { shallow } from "enzyme";
 
 describe("SearchBox", () => {
   const mockCallback = jest.fn().mockName("SearchOnChange");
@@ -10,5 +11,23 @@ describe("SearchBox", () => {
 
     ReactDOM.render(<SearchBox onChange={mockCallback} value="" />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders correctly", () => {
+    const sut = shallow(
+      <SearchBox onChange={mockCallback} value="Current Search" />
+    );
+
+    expect(sut).toMatchInlineSnapshot(`
+      <styled.div>
+        <styled.input
+          aria-label="Search list of characters"
+          defaultValue="Current Search"
+          onChange={[Function]}
+          placeholder="Search"
+          type="search"
+        />
+      </styled.div>
+    `);
   });
 });
